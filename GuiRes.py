411dac9f7616e63b3d7f04c5e53c8a3443f0a7db
William Faircloth <89104800+Kidtruck91@users.noreply.github.com>
2024-11-20 23:25:12 -0500

    Update GuiRes.py

--- GuiRes.py
+++ GuiRes.py
@@ -1,17 +1,17 @@
 # Dictionary mapping card values to image paths
 CARD_IMAGES = {
-    1: "images/cards/ace.png",   # Ace
-    2: "images/cards/two.png",
-    3: "images/cards/three.png",
-    4: "images/cards/four.png",
-    5: "images/cards/five.png",
-    6: "images/cards/six.png",
-    7: "images/cards/seven.png",
-    8: "images/cards/eight.png",
-    9: "images/cards/nine.png",
-    10: "images/cards/ten.png",
-    "J": "images/cards/jack.png",   # Jack
-    "Q": "images/cards/queen.png",  # Queen
-    "K": "images/cards/king.png",   # King
-    "?": "images/cards/hidden.png"  # Hidden card
+    1: "./cards/14_of_clubs.png",   # Ace
+    2: "./cards/2_of_clubs.png",
+    3: "./cards/3_of_clubs.png",
+    4: "./cards/4_of_clubs.png",
+    5: "./cards/5_of_clubs.png",
+    6: "./cards/6_of_clubs.png",
+    7: "./cards/7_of_clubs.png",
+    8: "./cards/8_of_clubs.png",
+    9: "./cards/9_of_clubs.png",
+    10: "./10_of_clubs.png",
+    "J": "./cards/11_of_clubs.png",   # Jack
+    "Q": "./cards/12_of_clubs.png",  # Queen
+    "K": "./cards/13_of_clubs.png"   # King
+    #"?": "./cards/hidden.png"  # Hidden card
 }
