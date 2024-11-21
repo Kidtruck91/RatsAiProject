3429913564bfe57e64ff21c8f6e6f719db5d539b
William Faircloth <89104800+Kidtruck91@users.noreply.github.com>
2024-11-20 23:15:49 -0500

    Create GuiRes.py

---
+++ GuiRes.py
@@ -0,0 +1,17 @@
+# Dictionary mapping card values to image paths
+CARD_IMAGES = {
+    1: "images/cards/ace.png",   # Ace
+    2: "images/cards/two.png",
+    3: "images/cards/three.png",
+    4: "images/cards/four.png",
+    5: "images/cards/five.png",
+    6: "images/cards/six.png",
+    7: "images/cards/seven.png",
+    8: "images/cards/eight.png",
+    9: "images/cards/nine.png",
+    10: "images/cards/ten.png",
+    "J": "images/cards/jack.png",   # Jack
+    "Q": "images/cards/queen.png",  # Queen
+    "K": "images/cards/king.png",   # King
+    "?": "images/cards/hidden.png"  # Hidden card
+}
